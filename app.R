d8b6a5d4131ac47b386bdc951dc8df33c2b6c097
Nickolas Spencer <ocvipers11@gmail.com>
2021-04-30 01:22:36 +0000

    final version of project

--- app.R
+++ app.R
@@ -8,7 +8,11 @@ library(plotly)
 library(reshape2)
 library(tidyr)
 library(dplyr)
+library(bslib)
+#install.packages("thematic")
+#library(thematic)
 
+## Making Hitter Stats
 batting2 <- Batting %>% 
   select(playerID, yearID,teamID, H, X2B, X3B, HR) %>%
   filter(yearID %in% c("2015","2016")) %>% 
@@ -20,9 +24,12 @@ batting2 <- Batting %>%
   mutate(Triple = X3B) %>% 
   mutate(HomeRun = HR)
 
+
+## Slimming down Hitters Stats
 bat2 <- batting2 %>% 
   select(teamID, yearID, Single, Double, Triple, HomeRun)
 
+## Making it easier for ploting
 bat_long2 <- melt(bat2, id.vars = c("playerID","teamID","yearID"))
 
 newteam <- Teams %>% 
@@ -35,6 +42,7 @@ batmaster <- inner_join(bat_long2, Master, by = "playerID") %>%
   select("teamID", "yearID", "nameGiven", "variable", "value")
 Batmaster <- inner_join(batmaster, newteam, by = "teamID")
 
+## Getting Rid of retired team names
 Teams1 <- bat_long3 %>% 
   group_by(name) %>% 
   summarise() %>% 
@@ -44,8 +52,11 @@ Teams1 <- bat_long3 %>%
                     "New York Highlanders","Philadelphia Quakers","Philadelphia Blue Jays"
                     ,"Pittsburg Alleghenys","St. Louis Browns","St. Louis Perfectos",
                     "Washington Senators","Tampa Bay Devil Rays"))
+
+## Making Year selector Option 
 Years <- seq(2015,2016,1)
 
+## Making Salaries List
 money <- Salaries %>% 
   filter(yearID %in% c(2015,2016)) %>% 
   group_by(teamID, yearID, playerID) %>% 
@@ -61,95 +72,146 @@ mastersalaries <- inner_join(salaries1,Master, by = "playerID") %>%
                       ,"Pittsburg Alleghenys","St. Louis Browns","St. Louis Perfectos",
                       "Washington Senators","Tampa Bay Devil Rays"))
 
-nameteam <- mastersalaries %>% 
+## Making Players Names for options
+nameplayers<- mastersalaries %>% 
   group_by(nameGiven) %>% 
   summarise() 
 
+## Updated Batmaster for interactive graph 1 and 2
+TeamHits <- Batmaster %>% 
+  group_by(yearID, name, variable) %>% 
+  summarise(value = sum(value))
 
 
 ui <-fluidPage(
-  headerPanel("Hit's Counter : MLB Teams"),
-  helpText("This Shiny App will let you look at whhat kind of hits a MLB teams had in a single year!"),
+  headerPanel("MLB Analysis: Salary"),
+  helpText("Select the Options below to Compare Salaries with Batting Stats"),
+  theme = shinythemes::shinytheme("yeti"),
   
-  fluidRow( column(
-    width = 6,
-    selectInput("team","Teams:",Teams1),
+ 
+
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Three options below are for selection 1:"),
+  selectInput("team","Teams:",Teams1),
   selectInput("year", "Years:",Years),
-  selectInput("player", "Player:", nameteam)),
-  
-  column(
-    width =6,
+  uiOutput("player1"),
+  helpText("Three options below are for selection 2:"),
     selectInput("team2","Teams:",Teams1),
-  selectInput("year2", "Years:",Years)),
+  selectInput("year2", "Years:",Years),
+  uiOutput("player2"),
   
-  
-  actionButton("goButton", "Go!"),
+  actionButton("goButton", "Compare MLB Stats!"),
+  width = 3),
   
   
   
   
  fluidRow(
    mainPanel(
-    column(6,plotOutput("plot1")),
-    column(6,plotOutput("plot2")),
-    column(6,plotOutput("plot3")),
-    column(6,plotOutput("plot4"))
+    column(6,plotlyOutput("plot1")),
+    column(6,plotlyOutput("plot2")),
+    column(6,plotlyOutput("plot3")),
+    column(6,plotlyOutput("plot4")),
+    column(6,plotlyOutput("plot5")),
+    column(6,plotlyOutput("plot6"))
   
   ))))
 
 server <- function(input, output, session) {
   
+  
   values <- reactiveValues(
-    a = "Arizona Diamondbacks",
-    b = "2015",
-    c = "David Jose",
-    d = "Baltimore Orioles",
-    e = "2015"
+    a = NULL,
+    b = NULL,
+    c = NULL,
+    d = NULL,
+    e = NULL
     
   )
-  
-  
-  
   observeEvent(input$goButton, {
     values$a<- input$team
     values$b<- input$year
     values$c<- input$player
     values$d<- input$team2
     values$e<- input$year2
+    values$f<- input$player2
   })
   
-   output$plot1 <- renderPlot({
-    Batmaster %>% 
+  output$player1 <- renderUI({
+    req(input$team, input$year)
+    player_choices <- mastersalaries %>% 
+      filter(name == input$team & yearID == input$year) %>% 
+      pull(nameGiven)
+    selectInput("player", "Player:",
+                choices = player_choices)})
+  
+  output$player2 <- renderUI({
+    req(input$team2, input$year2)
+    player_choices <- mastersalaries %>% 
+      filter(name == input$team2 & yearID == input$year2) %>% 
+      pull(nameGiven)
+    selectInput("player2", "Player:",
+                choices = player_choices)})
+    
+   output$plot1 <- renderPlotly({
+     req(values$a, values$b, values$c, values$d, values$e,values$f )
+    TeamHits %>% 
     filter(name == values$a) %>% 
     filter(yearID == values$b) %>% 
       ggplot(aes(x = variable, y = value,text = paste("Type of Hit =", variable, "\nAmount of Hits =", value)))+
       geom_bar(stat = "identity", aes(fill = variable))+
+      theme(legend.position = "none")+
       labs(x = "", y = "", title = paste(values$a,",",values$b))})
   
-  output$plot2 <- renderPlot({
-    bat_long3 %>% 
-      filter(name == input$team2) %>% 
-      filter(yearID == input$year2) %>% 
+  output$plot2 <- renderPlotly({
+    req(values$a, values$b, values$c, values$d, values$e,values$f)
+    TeamHits %>% 
+      filter(name == values$d) %>% 
+      filter(yearID == values$e) %>% 
       ggplot(aes(x = variable, y = value,text = paste("Type of Hit =", variable, "\nAmount of Hits =", value)))+
       geom_bar(stat = "identity", aes(fill = variable))+
-      labs(x = "", y = "", title = paste(input$team2,",",input$year2))})
+      theme(legend.position = "none")+
+      labs(x = "", y = "", title = paste(values$d,",",values$e))})
   
-  output$plot3 <- renderPlot({
+  output$plot3 <- renderPlotly({
+    req(values$a, values$b, values$c, values$d, values$e,values$f)
     mastersalaries %>% 
-      filter(name == input$team & yearID == input$year) %>% 
+      filter(name == values$a & yearID == values$b) %>% 
       ggplot(aes(x = nameGiven, y = salary))+
       geom_bar(stat = "identity", fill = "orange")+
       theme(axis.text.x = element_text(angle = 90, vjust = 0.5))+
-      labs(x = "Players", y ="Salary" , title = paste(input$team,",",input$year))
+      labs(x = "Players", y ="Salary" , title = paste(values$a,",",values$b))
    }) 
   
-  output$plot4 <- renderPlot({
+  output$plot4 <- renderPlotly({
+    req(values$a, values$b, values$c, values$d, values$e,values$f)
+    mastersalaries %>% 
+      filter(name == values$d & yearID == values$e) %>% 
+      ggplot(aes(x = nameGiven, y = salary))+
+      geom_bar(stat = "identity", fill = "orange")+
+      theme(axis.text.x = element_text(angle = 90, vjust = 0.5))+
+      labs(x = "Players", y ="Salary" , title = paste(values$d,",",values$e))
+  }) 
+  
+  output$plot5 <- renderPlotly({
+    req(values$a, values$b, values$c, values$d, values$e,values$f)
     Batmaster %>% 
       filter(name == values$a  & nameGiven == values$c & yearID == values$b) %>% 
       ggplot(aes(x = variable, y = value,text = paste("Type of Hit =", variable, "\nAmount of Hits =", value)))+
-      geom_bar(stat = "identity", aes(fill = variable))
-    
-  
+      geom_bar(stat = "identity", aes(fill = variable))+
+      theme(legend.position = "none")+
+      labs(x = "", y ="" , title = paste("Batting Stats for", values$c,"for",values$a,"in", values$b))
+    })
+  output$plot6 <- renderPlotly({
+    req(values$a, values$b, values$c, values$d, values$e,values$f)
+    Batmaster %>% 
+      filter(name == values$d  & nameGiven == values$f & yearID == values$e) %>% 
+      ggplot(aes(x = variable, y = value,text = paste("Type of Hit =", variable, "\nAmount of Hits =", value)))+
+      geom_bar(stat = "identity", aes(fill = variable))+
+      theme(legend.position = "none")+
+      labs(x = "", y ="" , title = paste("Batting Stats for", values$f,"for",values$d, "in", values$e))
   })
 }
 
